7b3a34a634ad476567c7063aee2d4d3dac164e3e
Kai_Xuan233 <likaiyue0989@foxmail.com>
2020-11-22 22:29:39 +0800

    fix bugs

--- src/main/java/net/lawaxi/serverbase/mixin/CommandManagerMixin.java
+++ src/main/java/net/lawaxi/serverbase/mixin/CommandManagerMixin.java
@@ -4,6 +4,7 @@ package net.lawaxi.serverbase.mixin;
 import com.mojang.brigadier.CommandDispatcher;
 import net.lawaxi.serverbase.commands.*;
 import net.lawaxi.serverbase.utils.config.configs;
+import net.lawaxi.serverbase.utils.config.messages;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,6 +28,8 @@ public class CommandManagerMixin {
         if (!configfolder.exists())
             configfolder.mkdir();
 
+        new configs();
+        new messages();
 
         CommandDispatcher<ServerCommandSource> dispatcher = ((CommandManager) (Object) this).getDispatcher();
 
